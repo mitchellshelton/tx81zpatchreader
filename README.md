511c7335b5a277b08fe9346c0782c9e04d44874b
Mitchell Shelton <shelton.5@osu.edu>
2014-07-27 12:14:24 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TX81Z Patch Name Reader
+
+---
+
+> A simple TX81Z patch name reader. The file format is somewhat specific. It looks for a string of characters (ccc222) after each patch name. I have found some patches that do not have these characters. I may expand this project and learn a little more about the format in the future. For now, my dad was going through and copying these names out of a hex editor, so I just threw this together.
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+if (!$_POST) {
+
+	$filename = $_SERVER["PHP_SELF"];
+	print '<form method="post" action="' . $filename . '" enctype="multipart/form-data">';
+
+?>
+  <p>
+  	<label for="filename">Upload a TX81Z compatible sysex file (.syx files only): </label>
+  	<input type="file" name="filename" id="filename" />
+  </p>
+  <input type="submit" name="submit" value="Parse">
+<?php
+
+	print '</form>';
+
+} else {
+
+	$allowedExts = array("syx");
+	$temp = explode(".", $_FILES["filename"]["name"]);
+	$extension = end($temp);
+
+	if (in_array($extension, $allowedExts) && ($_FILES["filename"]["size"] < 200000)) {
+		if ($_FILES["filename"]["error"] > 0) {
+	  	echo "Error: " . $_FILES["filename"]["error"] . "<br />";
+	  	break;
+		} else {
+	  	echo "Results from " . $_FILES["filename"]["name"] . "<hr />";
+		}
+	} else {
+	  echo "Invalid file";
+	  break;
+	}
+
+	$filename = $_FILES["filename"]["tmp_name"];
+	$rawpatch = file_get_contents($filename);
+	$patch = bin2hex($rawpatch);
+	$chars = array();
+	preg_match_all("/\w{20}(?=636363323232)/", $patch, $chars);
+
+	foreach($chars as $val) {
+		foreach($val as $key => $voice) {
+			$num = sprintf("%02d.\n", $key+1);
+			$asciiversion = pack("H*" , $voice);
+	    print $num . htmlentities($asciiversion) . '<br />';
+	  }
+	}
+
+}
+
+?>
