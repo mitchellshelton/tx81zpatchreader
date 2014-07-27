bdd3b74f3107e6c3aec5fafcdb52a8436302b1f8
Mitchell Shelton <shelton.5@osu.edu>
2014-07-27 12:26:15 -0400

    Comments and general cleanup

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 if (!$_POST) {
 
+	// self posting form
 	$filename = $_SERVER["PHP_SELF"];
 	print '<form method="post" action="' . $filename . '" enctype="multipart/form-data">';
 
@@ -17,11 +18,14 @@ if (!$_POST) {
 
 } else {
 
-	$allowedExts = array("syx");
-	$temp = explode(".", $_FILES["filename"]["name"]);
-	$extension = end($temp);
+	// Specify out extension
+	$extensions = array("syx");
+	$tempfilename = explode(".", $_FILES["filename"]["name"]);
+	$ext = end($tempfilename);
 
-	if (in_array($extension, $allowedExts) && ($_FILES["filename"]["size"] < 200000)) {
+	// Prevent large or non-syx files from being fed into the system
+	if (in_array($ext, $extensions) && ($_FILES["filename"]["size"] < 200000)) {
+		// Confirm that our file was uploaded
 		if ($_FILES["filename"]["error"] > 0) {
 	  	echo "Error: " . $_FILES["filename"]["error"] . "<br />";
 	  	break;
@@ -29,20 +33,29 @@ if (!$_POST) {
 	  	echo "Results from " . $_FILES["filename"]["name"] . "<hr />";
 		}
 	} else {
-	  echo "Invalid file";
+	  echo "This file is unreadable by this system.";
 	  break;
 	}
 
+	// Get the location of our uploaded file
 	$filename = $_FILES["filename"]["tmp_name"];
+	// Grab the file contents (note that this converts the hex to ascii automatically)
 	$rawpatch = file_get_contents($filename);
+	// Convert the contents back into hex
 	$patch = bin2hex($rawpatch);
+	// Initialize an array
 	$chars = array();
+	// Get the 20 hex characters before our ccc222 pattern
 	preg_match_all("/\w{20}(?=636363323232)/", $patch, $chars);
 
+	// Loop over the array of voice names
 	foreach($chars[0] as $key => $voice) {
-			$num = sprintf("%02d.\n", $key+1);
-			$asciiversion = pack("H*" , $voice);
-	    print $num . htmlentities($asciiversion) . '<br />';
+		// Add a leading zero to the voice names
+		$num = sprintf("%02d.\n", $key+1);
+		// Convert the voice name from hex to ascii
+		$asciiversion = pack("H*" , $voice);
+		// Display the name
+	  print $num . htmlentities($asciiversion) . '<br />';
 	}
 
 }
